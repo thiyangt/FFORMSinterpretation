7e8d86f5ca27a83ed44905684c1572f6e5b556ab
thiyangt <thiyanga.talagala@monash.edu>
2019-05-22 17:41:53 +1000

    add Friedman's H-statistic calculation files using explainer

---
+++ src/interaction_cluster/FriedmanHstatcalculation_usingexplainer/y_interactions_explainer.sh
@@ -0,0 +1,11 @@
+#!/bin/env bash
+#SBATCH --job-name=yFriedExplainer
+#SBATCH --time=10:00:00
+#SBATCH --mem=50G
+#SBATCH --ntasks=1
+#SBATCH --partition=short
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=thiyanga.talagala@monash.edu
+#SBATCH --output=y_interactions_explainer.txt
+module load R/3.5.1
+R --vanilla < y_interactions_explainer.R > y_interactions_explainer.txt
