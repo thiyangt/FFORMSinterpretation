7a19147a5b96b20e3995ece0a50adac983b3ef87
thiyangt <thiyanga.talagala@monash.edu>
2019-10-18 11:55:32 +1100

    add table 4

---
+++ src/modelcounttable.R
@@ -0,0 +1,25 @@
+## Appendix C table
+
+## yearly
+load("data/predictedCLM4Y.rda")
+table(predictedCLM4Y)
+
+## quarterly
+load("data/predictedCLM4Q.rda")
+table(predictedCLM4Q)
+
+## monthly
+load("data/predictedCLM4M.rda")
+table(predictedCLM4M)
+
+## weekly
+load("data/predictedCLM4W.rda")
+table(predictedCLM4W)
+
+## daily
+load("data/predictedCLM4D.rda")
+table(predictedCLM4D)
+
+##
+load("data/predictedCLM4H.rda")
+table(predictedCLM4H)
